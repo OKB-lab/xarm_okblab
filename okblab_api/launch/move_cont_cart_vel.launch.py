4b5d5eb593b45a3fa32f8b488fe3d2efe9d961c3
Kousuke-Okabe <kousuke.okabe@ieee.org>
2026-07-08 13:08:33 +0900

    move_cont_cart_vel設定の修正とxarm6_activate.launchの統合

--- okblab_api/launch/move_cont_cart_vel.launch.py
+++ okblab_api/launch/move_cont_cart_vel.launch.py
@@ -1,10 +1,25 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    xarm6_activate = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare('okblab_api'),
+                'launch',
+                'xarm6_activate.launch.py',
+            ])
+        ),
+        launch_arguments={
+            'mode': '5',
+        }.items(),
+    )
+
     parameters_file = PathJoinSubstitution([
         FindPackageShare('okblab_api'),
         'config',
@@ -12,6 +27,7 @@ def generate_launch_description():
     ])
 
     return LaunchDescription([
+        xarm6_activate,
         Node(
             package='okblab_api',
             executable='move_cont_cart_vel',
